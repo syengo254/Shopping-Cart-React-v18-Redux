cd9f9ff013c156b76f3b43bfa13459fb210e0704
David Syengo <david.syengo019@gmail.com>
2022-04-29 23:31:58 +0300

    UI done

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HistoryPage = () => {
     return ( 
         <React.Fragment>
-            
+            {/* display a list of previous orders if any */}
         </React.Fragment>
      );
 }
